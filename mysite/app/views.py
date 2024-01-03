963cb7b3850eec4dca33f59089ce5b76d450e7f8
gunayy <gunayhamzayava@gmail.com>
2024-01-04 00:29:15 +0400

    tutorial-changed

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "CoreyMs",
+        "title": "Blog Post 1",
+        "content": "First Post",
+        "data_posted": "August 2015",
+    },
+    {
+        "author": "lorem",
+        "title": "Blog Post 2",
+        "content": "First Post",
+        "data_posted": "August 2015",
+    },
+    {
+        "author": "lorem3",
+        "title": "Blog Post 3",
+        "content": "First Post",
+        "data_posted": "August 2015",
+    },
+    {
+        "author": "lorem3",
+        "title": "Blog Post 4",
+        "content": "First Post",
+        "data_posted": "August 2015",
+    },
+]
 
-# Create your views here.
+
 def home(request):
-    return render(request,'blog/home.html')
+    return render(request, "blog/home.html")
 
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request, "blog/about.html")
