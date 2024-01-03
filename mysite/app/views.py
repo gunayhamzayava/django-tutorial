c0d9d94f2832a3155903850f256f9e109b211fe4
gunayy <gunayhamzayava@gmail.com>
2024-01-03 23:41:48 +0400

    tutorial-changed

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
+# Create your views here.
 def home(request):
-    return HttpResponse('<h1>Salammm</h1>')
+    return HttpResponse("<doctype>...")
+
+
+def about(request):
+    return HttpResponse("<h1>About us</h1>")
