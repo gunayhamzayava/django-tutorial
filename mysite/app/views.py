0130fe6ec50a03888e9b4ad01b9c49af489c2a80
gunayy <gunayhamzayava@gmail.com>
2024-01-04 00:12:34 +0400

    tutorial-changed

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<doctype>...")
+    return render(request,'blog/home.html')
 
 
 def about(request):
